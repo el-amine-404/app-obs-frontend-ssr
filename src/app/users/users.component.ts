9dad7770609e796da11e90f54e885f09f8971734
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-07-09 13:31:19 +0100

    remove unused imports and commented code

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
-import { PostService } from '../services/post.service';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -12,30 +10,13 @@ import { UserService } from '../services/user.service';
 })
 export class UsersComponent implements OnInit {
 
-  // http = inject(HttpClient);
   private userService = inject(UserService);
   users: any = [];
 
   ngOnInit(): void {
-    // this.fetchUsers();
     this.loadUsers();
   }
 
-  // fetchUsers(){
-  //   this.http.get('https://jsonplaceholder.typicode.com/users')
-  //     .subscribe((users: any) => {
-  //       console.log(users);
-
-  //       this.users = users;
-  //     });
-  // }
-
-  // loadUsers(){
-  //   this.userService.getUsers().subscribe((users: any) => {
-  //     console.log(users);
-  //     this.users = users;
-  //   })
-  // }
   loadUsers(){
     this.userService.getUsers().subscribe({
       next: (users: any) =>{
